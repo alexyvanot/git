b912d6c5bfba11b75d87e772ed24a6ee219f4e9e
alexyvanot <alexystefen@hotmail.com>
2022-04-19 15:29:20 +0200

    Little modifications...

--- index.js
+++ index.js
@@ -143,6 +143,15 @@ function isSumNull() {
 	}
 }
 
+/*
+* Unusefull function to do nothing and return nothing.
+*
+* @return null
+*/
+function unusedFunction() {
+	return null;
+}
+
 
 var resultOfSum = isSumNull();
 isPrimeNumber();
